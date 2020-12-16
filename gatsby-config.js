ab83a0372997871e0f3fb2ce1053ed0840802de8
Geoffrey <info@runningdeveloper.com>
2020-12-16 22:29:47 +0200

    try deploy again

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,14 +19,14 @@ module.exports = {
     "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
+    // {
+    //   resolve: "gatsby-source-filesystem",
+    //   options: {
+    //     name: "images",
+    //     path: "./src/images/",
+    //   },
+    //   __key: "images",
+    // },
     {
       resolve: "gatsby-source-filesystem",
       options: {
