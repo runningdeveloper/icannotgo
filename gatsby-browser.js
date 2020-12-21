949fb4f03193dbb16a8d877f2db068b722da4321
Geoffrey <info@runningdeveloper.com>
2020-12-21 15:57:57 +0200

    button size + indication copied

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,7 +5,7 @@ import { MDXProvider } from "@mdx-js/react"
 const MyParagraph = props => (
   <p className="text-lg text-center" {...props} />
 )
-const MyH1 = props => <h1 className="text-3xl text-center mb-6 mt-2" {...props} />
+const MyH1 = props => <p className="text-3xl text-center mb-6 mt-2" {...props} />
 
 const MyHref = props => <a className="text-orange-700 font-semibold" {...props} />
 
--- src/components/share-line.js
+++ src/components/share-line.js
@@ -1,22 +1,27 @@
 import * as React from "react";
 
 const ShareLine = ({ children }) => {
+  const [copied, setCopied] = React.useState(false);
   return (
     <div className="mt-4 p-4 shadow-lg border-black border rounded-md text-center min-w-min">
       <div className="mt-2 flex flex-col">
         <span className="text-lg">{children}</span>
         <div className="mt-4">
           <button
-            onClick={() => navigator.clipboard.writeText(`${children}`)}
-            className="py-2 px-4 bg-teal-500 text-white font-semibold rounded-lg shadow-md hover:bg-teal-700 focus:outline-none"
+            onClick={() => {
+              navigator.clipboard.writeText(`${children}`);
+              setCopied(true);
+            }}
+            className="py-2 px-4 bg-teal-500 text-white font-semibold rounded-lg shadow-md hover:bg-teal-700 focus:outline-none text-base"
           >
-            Copy
+            {copied ? "Copied to clipboard" : "Copy"}
           </button>{" "}
           <a
             href={`https://twitter.com/share?url=${
               typeof window !== `undefined` ? window.location.href : ""
             }&text=${encodeURIComponent(children)}&via=geoff4l`}
-            className="py-2 px-4 bg-teal-500 text-white font-semibold rounded-lg shadow-md hover:bg-teal-700 focus:outline-none"
+            style={{ appearance: "auto" }}
+            className="py-2 px-4 bg-teal-500 text-white font-semibold rounded-lg shadow-md hover:bg-teal-700 focus:outline-none text-base"
           >
             Tweet
           </a>
